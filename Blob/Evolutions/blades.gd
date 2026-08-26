5caae48b668dce5b595250d183f3168a98540f79
LiamFricker <48845554+LiamFricker@users.noreply.github.com>
2026-08-26 01:12:34 -0700

    Blade and Spear Prototyping
    Not much here, but it's a start before I start getting down on this.
    Blade will be first. Pretty standard, just need to develop the rust mechanic.
    Spear will be even simpler at first. Just need to copy over Blade code. Spear will get more complicated with its DoT and Poison. Then finally its 3 path upgrade.

---
+++ Blob/Evolutions/blades.gd
@@ -0,0 +1,30 @@
+extends "res://Blob/Evolutions/base_evo_general.gd"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_detection_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+func _on_detection_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_hitbox_l_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+func _on_hitbox_l_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_hitbox_r_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+func _on_hitbox_r_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
