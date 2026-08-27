b38f1302cb469aa6ea7214a4b1007a0693c8eed6
LiamFricker <48845554+LiamFricker@users.noreply.github.com>
2026-08-27 02:25:02 -0700

    Blade Code Done
    Blade still needs the derust animation and particle effect.
    Blade has size compatibility. Need to ship this to other evos too.
    Things to test:
    - Make sure Blade Level change works fine, whether or not you need blade temp
    - connect Attack with attack in main, maybe make a for loop for this.
    - check detect and make sure the hitboxes look alright

--- Blob/Evolutions/base_evo_general.gd
+++ Blob/Evolutions/base_evo_general.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 @export var parentRef : Node2D
+@export var size : float = 1.0
 
 signal damagedEnemy(amt : float)
 
 func _damagedEnemy(amt : float) -> void:
 	damagedEnemy.emit(amt)
+
+func _ready() -> void:
+	_setSize()
+
+func _setSize() -> void:
+	pass
 
 func getID() -> int:
 	return parentRef.getID()
--- Blob/Evolutions/blades.gd
+++ Blob/Evolutions/blades.gd
@@ -9,123 +9,365 @@ var detected : bool = false
 
 @onready var hitbox_l_ref = $HitboxL
 @onready var hitbox_r_ref = $HitboxR
+#@onready var hitbox_ref
 
 var blade_tween
-var rust_tween
+var rust_tween_l
+var rust_tween_r
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var rust_max : float = 10.0
+var rust_level_l : float = 0.0
+var rust_level_r : float = 0.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var rust_recovery_speed : float = 1.0
 
+@export var startBladeColor : Color
+@export var endBladeColor : Color
+
+@export var pinch_cooldown : float = 2.0
 
 func setBladeLevel(newBL : int) -> void:
 	match blade_level:
 		0:
 			$HitboxL/CollisionShape2D.set_deferred("disabled", true)
 			$HitboxR/CollisionShape2D.set_deferred("disabled", true)
+			$Hitbox/CollisionShape2D.set_deferred("disabled", true)
+			$Detection/CollisionShape2D.set_deferred("disabled", true)
+			$Blade1.hide()
 		1:
 			$HitboxL/CollisionShape2D2.set_deferred("disabled", true)
 			$HitboxR/CollisionShape2D2.set_deferred("disabled", true)
+			$Hitbox/CollisionShape2D2.set_deferred("disabled", true)
+			$Detection/CollisionShape2D2.set_deferred("disabled", true)
+			$Blade2.hide()
 		2:
 			$HitboxL/CollisionShape2D3.set_deferred("disabled", true)
 			$HitboxR/CollisionShape2D3.set_deferred("disabled", true)
+			$Hitbox/CollisionShape2D3.set_deferred("disabled", true)
+			$Detection/CollisionShape2D3.set_deferred("disabled", true)
+			$Blade3.hide()
 	
 	blade_level = newBL
+	rust_level_l = 0.0
+	rust_level_r = 0.0
+	if rust_tween_l:
+		rust_tween_l.kill()
+	if rust_tween_r:
+		rust_tween_r.kill()
 	
-	match blade_level:
-		0:
-			$HitboxL/CollisionShape2D.set_deferred("disabled", false)
-			$HitboxR/CollisionShape2D.set_deferred("disabled", false)
-		1:
-			$HitboxL/CollisionShape2D2.set_deferred("disabled", false)
-			$HitboxR/CollisionShape2D2.set_deferred("disabled", false)
-		2:
-			$HitboxL/CollisionShape2D3.set_deferred("disabled", false)
-			$HitboxR/CollisionShape2D3.set_deferred("disabled", false)
+	if not detected:
+		match blade_level:
+			0:
+				$HitboxL/CollisionShape2D.set_deferred("disabled", false)
+				$HitboxR/CollisionShape2D.set_deferred("disabled", false)
+				$Hitbox/CollisionShape2D.set_deferred("disabled", false)
+				$Detection/CollisionShape2D.set_deferred("disabled", false)
+				$Blade1.show()
+			1:
+				$HitboxL/CollisionShape2D2.set_deferred("disabled", false)
+				$HitboxR/CollisionShape2D2.set_deferred("disabled", false)
+				$Hitbox/CollisionShape2D2.set_deferred("disabled", false)
+				$Detection/CollisionShape2D.set_deferred("disabled", false)
+				$Blade2.show()
+			2:
+				$HitboxL/CollisionShape2D3.set_deferred("disabled", false)
+				$HitboxR/CollisionShape2D3.set_deferred("disabled", false)
+				$Hitbox/CollisionShape2D3.set_deferred("disabled", false)
+				$Detection/CollisionShape2D.set_deferred("disabled", false)
+				$Blade3.show()
 	
 	
 
 func _on_detection_area_entered(_area: Area2D) -> void:
 	if not detected and not attacking:
-		detected = true
-		_pinch()
+		if rust_level_l < rust_max or rust_level_r < rust_max:
+			detected = true
+			_pinch()
 		
 
 func _on_detection_body_entered(_body: Node2D) -> void:
 	if not detected and not attacking:
 		detected = true
-		_pinch()
+		if rust_level_l < rust_max or rust_level_r < rust_max:
+			_pinch()
 
 func _detectionCheck() -> void:
-	var detectNode = $Detection
+	var detectNode = $Hitbox
 	
 	if (detectNode.has_overlapping_areas() or detectNode.has_overlapping_bodies()):
-		if not detected and not attacking:
+		if not attacking:
 			detected = true
-			_pinch()
+			if rust_level_l < rust_max or rust_level_r < rust_max:
+				_pinch()
+	detected = false
+
+func _BLtoColShape(BL : int) -> CollisionShape2D:
+	match BL:
+		0:
+			return $Hitbox/CollisionShape2D
+		1:
+			return $Hitbox/CollisionShape2D2
+		_:
+			return $Hitbox/CollisionShape2D3
+
+func _toggle(toggle : bool) -> void:
+	if rust_level_l < rust_max: #not toggle or 
+		#hitbox_l_ref.set_deferred("monitoring", toggle)
+		hitbox_l_ref.set_deferred("monitorable", toggle)
+	if rust_level_r < rust_max: #not toggle or 
+		#hitbox_r_ref.set_deferred("monitoring", toggle)
+		hitbox_r_ref.set_deferred("monitorable", toggle)
 
 func _pinch() -> void:
 	var pinch_time = 1.5
 	
+	_BLtoColShape(blade_level).set_deferred("disabled", false)
+	#_toggle(false)
+	
 	if blade_tween:
 		blade_tween.kill()
-	blade_tween = create_tween()
-	blade_tween.tween_property(blade_left_refs[blade_level], "rotation", PI/4, pinch_time)
-	blade_tween.parallel().tween_property(blade_right_refs[blade_level], "rotation", -PI/4, pinch_time)
-	blade_tween.parallel().tween_property(hitbox_l_ref, "rotation", -PI/4, pinch_time)
-	blade_tween.parallel().tween_property(hitbox_r_ref, "rotation", -PI/4, pinch_time)
+	blade_tween = create_tween().set_parallel()
+	if rust_level_l < rust_max:
+		blade_tween.tween_property(blade_left_refs[blade_level], "rotation", PI/4, pinch_time)
+		#blade_tween.parallel().tween_property(hitbox_l_ref, "rotation", -PI/4, pinch_time)
+		blade_tween.tween_property(blade_left_refs[blade_level], "rotation", 0, pinch_time).set_delay(pinch_time)
+		#blade_tween.parallel().tween_property(hitbox_l_ref, "rotation", 0, pinch_time)
+	if rust_level_r < rust_max:
+		blade_tween.tween_property(blade_right_refs[blade_level], "rotation", -PI/4, pinch_time)
+		#blade_tween.parallel().tween_property(hitbox_r_ref, "rotation", -PI/4, pinch_time)
+		blade_tween.tween_property(blade_right_refs[blade_level], "rotation", 0, pinch_time).set_delay(pinch_time)
+		#blade_tween.parallel().tween_property(hitbox_r_ref, "rotation", 0, pinch_time)
 	
-	
-	blade_tween.tween_property(blade_left_refs[blade_level], "rotation", 0, pinch_time)
-	blade_tween.parallel().tween_property(blade_right_refs[blade_level], "rotation", 0, pinch_time)
-	blade_tween.parallel().tween_property(hitbox_l_ref, "rotation", 0, pinch_time)
-	blade_tween.parallel().tween_property(hitbox_r_ref, "rotation", 0, pinch_time)
-	
-	blade_tween.finished.connect(_pinchEnd)
+	#var blade_temp = blade_level
+	blade_tween.finished.connect(_pinchEnd.bind(blade_level))#blade_temp))
 
-func _pinchEnd() -> void:
-	detected = false
+func _pinchEnd(BL : int) -> void:
+	_BLtoColShape(BL).set_deferred("disabled", true)
+	#_toggle(true)
+	$PinchCooldown.start(pinch_cooldown)
+	
+func _on_pinch_cooldown_timeout() -> void:
 	_detectionCheck()
 
 func attack(temp : float, charge_cd : float) -> void:
-	if not attacking:
+	var rustL : bool = rust_level_l < rust_max
+	var rustR : bool = rust_level_r < rust_max
+	if not attacking and (rustL or rustR):
 		attacking = true
-		detected = true
 		
-		var base_time = 0.75
-		var scale_vec = Vector2(1.5, 1.0)
+		var charge_time : float = min(0.3 * charge_cd, 0.45)
+		var swipe_time : float = min(0.2 * charge_cd, 0.3)
+		var return_time : float = min(0.5 * charge_cd, 0.75)
+		var bladeLen = max(2.0 * temp/5.0, 1.5) 
 		
 		if blade_tween:
 			blade_tween.kill()
-		blade_tween = create_tween()
-		blade_tween.tween_property(blade_left_refs[blade_level], "rotation", -PI/8, base_time)
-		blade_tween.parallel().tween_property(blade_right_refs[blade_level], "rotation", PI/8, base_time)
-		blade_tween.parallel().tween_property(hitbox_l_ref, "rotation", -PI/8, base_time)
-		blade_tween.parallel().tween_property(hitbox_r_ref, "rotation", PI/8, base_time)
+		blade_tween = create_tween().set_parallel()
+		if rustL:
+			blade_tween.tween_property(blade_left_refs[blade_level], "scale:y", bladeLen, return_time)
+			blade_tween.tween_property(blade_left_refs[blade_level], "rotation", -PI/8, charge_time)
+			blade_tween.tween_property(blade_left_refs[blade_level], "rotation", PI/4, swipe_time).set_delay(charge_time)
+			blade_tween.tween_property(blade_left_refs[blade_level], "rotation", 0, return_time).set_delay(return_time)
+			blade_tween.tween_property(blade_left_refs[blade_level], "scale:y", 1.0, return_time).set_delay(return_time)
 		
-		blade_tween.tween_property(blade_left_refs[blade_level], "rotation", PI/4, base_time)
-		blade_tween.parallel().tween_property(blade_right_refs[blade_level], "rotation", -PI/4, base_time)
-		blade_tween.parallel().tween_property(hitbox_l_ref, "rotation", -PI/4, base_time)
-		blade_tween.parallel().tween_property(hitbox_r_ref, "rotation", -PI/4, base_time)
+		if rustR:
+			blade_tween.tween_property(blade_right_refs[blade_level], "scale:y", bladeLen, return_time)
+			blade_tween.tween_property(blade_right_refs[blade_level], "rotation", PI/8, charge_time)
+			blade_tween.tween_property(blade_right_refs[blade_level], "rotation", -PI/4, swipe_time).set_delay(charge_time)
+			blade_tween.tween_property(blade_right_refs[blade_level], "rotation", 0, return_time).set_delay(return_time)
+			blade_tween.tween_property(blade_left_refs[blade_level], "scale:y", 1.0, return_time).set_delay(return_time)
 		
-		blade_tween.tween_property(blade_left_refs[blade_level], "rotation", 0, base_time)
-		blade_tween.parallel().tween_property(blade_right_refs[blade_level], "rotation", 0, base_time)
-		blade_tween.parallel().tween_property(hitbox_l_ref, "rotation", 0, base_time)
-		blade_tween.parallel().tween_property(hitbox_r_ref, "rotation", 0, base_time)
+		#var blade_temp = blade_level
+		blade_tween.finished.connect(_attackEnd.bind(blade_level))#blade_temp))
+		
+		_colShapeIncrease(blade_level, bladeLen)
+		_BLtoColShape(blade_level).set_deferred("disabled", false)
+		
 
-func _on_hitbox_l_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _attackEnd(BL : int) -> void:
+	_BLtoColShape(BL).set_deferred("disabled", true)
+	_colShapeReset(BL)
+	if detected:
+		_detectionCheck()
 
-func _on_hitbox_l_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _colShapeIncrease(BL : int, inc : float) -> void:
+	var tempShape = CircleShape2D
+	var abs_size = size * inc
+	match BL:
+		0:
+			tempShape.radius = 17 * abs_size
+			$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+			$Hitbox/CollisionShape2D.set_deferred("position:y", -20*abs_size)
+		1:
+			tempShape.radius = 24 * size
+			$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+			$Hitbox/CollisionShape2D.set_deferred("position:y", -22*abs_size)
+		2:
+			tempShape.radius = 28 * size
+			$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+			$Hitbox/CollisionShape2D.set_deferred("position:y", -32*abs_size)
+
+func _colShapeReset(BL : int) -> void:
+	var tempShape = CircleShape2D
+	match BL:
+		0:
+			tempShape.radius = 17 * size
+			$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+			$Hitbox/CollisionShape2D.set_deferred("position:y", -20*size)
+		1:
+			tempShape.radius = 24 * size
+			$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+			$Hitbox/CollisionShape2D.set_deferred("position:y", -22*size)
+		2:
+			tempShape.radius = 28 * size
+			$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+			$Hitbox/CollisionShape2D.set_deferred("position:y", -32*size)
 
 
-func _on_hitbox_r_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _setSize() -> void:
+	var sizeVec = size * Vector2(1,1)
+	$Blade1.scale = sizeVec
+	$Blade2.scale = sizeVec
+	$Blade3.scale = sizeVec
+	
+	if attacking:
+		var tempShape = CircleShape2D
+		tempShape.radius = 17 * size
+		$Detection/CollisionShape2D.set_deferred("shape", tempShape)
+		$Detection/CollisionShape2D.set_deferred("position:y", -20*size)
+		
+		tempShape.radius = 24 * size
+		$Detection/CollisionShape2D.set_deferred("shape", tempShape)
+		$Detection/CollisionShape2D.set_deferred("position:y", -22*size)
+		
+		tempShape.radius = 28 * size
+		$Detection/CollisionShape2D.set_deferred("shape", tempShape)
+		$Detection/CollisionShape2D.set_deferred("position:y", -32*size)
+	else:
+		var tempShape = CircleShape2D
+		tempShape.radius = 17 * size
+		$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+		$Hitbox/CollisionShape2D.set_deferred("position:y", -20*size)
+		$Detection/CollisionShape2D.set_deferred("shape", tempShape)
+		$Detection/CollisionShape2D.set_deferred("position:y", -20*size)
+		
+		tempShape.radius = 24 * size
+		$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+		$Hitbox/CollisionShape2D.set_deferred("position:y", -22*size)
+		$Detection/CollisionShape2D.set_deferred("shape", tempShape)
+		$Detection/CollisionShape2D.set_deferred("position:y", -22*size)
+		
+		tempShape.radius = 28 * size
+		$Hitbox/CollisionShape2D.set_deferred("shape", tempShape)
+		$Hitbox/CollisionShape2D.set_deferred("position:y", -32*size)
+		$Detection/CollisionShape2D.set_deferred("shape", tempShape)
+		$Detection/CollisionShape2D.set_deferred("position:y", -32*size)
+	
+	var tempRect = RectangleShape2D
+	tempRect.size = size * Vector2(14.0, 38.0)
+	$HitboxL/CollisionShape2D.set_deferred("shape", tempRect)
+	$HitboxL/CollisionShape2D.set_deferred("position", Vector2(-15,-19)*size)
+	$HitboxR/CollisionShape2D.set_deferred("shape", tempRect)
+	$HitboxR/CollisionShape2D.set_deferred("position", Vector2(15,-19)*size)
+	
+	tempRect.size = size * Vector2(16.0, 46.0)
+	$HitboxL/CollisionShape2D.set_deferred("shape", tempRect)
+	$HitboxL/CollisionShape2D.set_deferred("position", Vector2(-18,-23)*size)
+	$HitboxR/CollisionShape2D.set_deferred("shape", tempRect)
+	$HitboxR/CollisionShape2D.set_deferred("position", Vector2(18,-23)*size)
+	
+	tempRect.size = size * Vector2(20.0, 60.0)
+	$HitboxL/CollisionShape2D.set_deferred("shape", tempRect)
+	$HitboxL/CollisionShape2D.set_deferred("position", Vector2(-20,-30)*size)
+	$HitboxR/CollisionShape2D.set_deferred("shape", tempRect)
+	$HitboxR/CollisionShape2D.set_deferred("position", Vector2(20,-30)*size)
 
-func _on_hitbox_r_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _damagedEnemyLeft(amt : float) -> void:
+	_damagedEnemy(amt)
+	_handleRustLeft(amt)
+
+func _handleRustLeft(amt : float) -> void:
+	if rust_level_l < rust_max:
+		rust_level_l += 1.0 * amt
+		var rust_length = 1.5 * min(amt / rust_max, 1.0)
+		var rust_level_diff = min(rust_level_l / rust_max, 1.0)
+		var end_rust_color = startBladeColor.lerp(endBladeColor, rust_level_diff)
+		
+		if rust_tween_l:
+			rust_tween_l.kill()
+		rust_tween_l = create_tween()
+		rust_tween_l.tween_property(blade_left_refs[blade_level], "modulate", end_rust_color, rust_length)
+		if rust_level_l < rust_max:
+			rust_tween_l.tween_interval(1.0 / rust_recovery_speed)
+			rust_tween_l.finished.connect(_deRustLeft)
+		else:
+			rust_tween_l.finished.connect(_RustedLeft)
+			#hitbox_l_ref.set_deferred("monitoring", false)
+			hitbox_l_ref.set_deferred("monitorable", false)
+	
+
+func _damagedEnemyRight(amt : float) -> void:
+	_damagedEnemy(amt)
+	_handleRustRight(amt)
+	
+func _handleRustRight(amt : float) -> void:
+	if rust_level_r < rust_max:
+		rust_level_r += 1.0 * amt
+		var rust_length = 1.5 * min(amt / rust_max, 1.0)
+		var rust_level_diff = min(rust_level_r / rust_max, 1.0)
+		var end_rust_color = startBladeColor.lerp(endBladeColor, rust_level_diff)
+		
+		if rust_tween_r:
+			rust_tween_r.kill()
+		rust_tween_r = create_tween()
+		rust_tween_r.tween_property(blade_right_refs[blade_level], "modulate", end_rust_color, rust_length)
+		if rust_level_r < rust_max:
+			rust_tween_r.tween_interval(1.0 / rust_recovery_speed)
+			rust_tween_r.finished.connect(_deRustRight)
+		else:
+			rust_tween_r.finished.connect(_RustedRight)
+			#hitbox_r_ref.set_deferred("monitoring", false)
+			hitbox_r_ref.set_deferred("monitorable", false)
+	
+func _damagedEnemyMid(amt : float) -> void:
+	_damagedEnemy(amt)
+	
+	_handleRustLeft(0.5*amt)
+	_handleRustRight(0.5*amt)
+
+func _RustedLeft() -> void:
+	$AnimationPlayer.play("removeRustLeft", -1, rust_recovery_speed)
+
+func _RustedRight() -> void:
+	$AnimationPlayer.play("removeRustRight", -1, rust_recovery_speed)
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	match anim_name:
+		"removeRustLeft":
+			rust_level_l = 0.0
+			hitbox_l_ref.set_deferred("monitorable", true)
+			if detected and not attacking:	
+				_detectionCheck()
+		"removeRustRight":
+			rust_level_r = 0.0
+			hitbox_r_ref.set_deferred("monitorable", true)
+			if detected and not attacking:	
+				_detectionCheck()
+
+func _deRustLeft() -> void:
+	var rust_length = 7.0 / rust_recovery_speed * min(rust_level_l / rust_max, 1.0)
+	
+	if rust_tween_l:
+		rust_tween_l.kill()
+	rust_tween_l = create_tween()
+	rust_tween_l.tween_property(blade_left_refs[blade_level], "modulate", startBladeColor, rust_length)
+	rust_tween_l.parallel().tween_property(self, "rust_level_l", 0, rust_length)
+
+func _deRustRight() -> void:
+	var rust_length = 7.0 / rust_recovery_speed * min(rust_level_r / rust_max, 1.0)
+	
+	if rust_tween_r:
+		rust_tween_r.kill()
+	rust_tween_r = create_tween()
+	rust_tween_r.tween_property(blade_right_refs[blade_level], "modulate", startBladeColor, rust_length)
+	rust_tween_r.parallel().tween_property(self, "rust_level_r", 0, rust_length)
